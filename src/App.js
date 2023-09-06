a1a8fe4fd64e0f34f5ae270b9f036b2cf1588b1d
Nitika <nkansal81@gmail.com>
2023-09-06 14:27:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import Comment from "./Comment/comment";
 
 const getNewComment = (commentValue, isRootNode = false, parentNodeId) => {
   return {
@@ -83,67 +83,5 @@ function App() {
   );
 }
 
-const Comment = ({ comment, addComment }) => {
-  const { commentText, childCommments, id } = comment;
-  const [childComment, setChildComment] = useState("");
-  const [show, setShow] = useState(true);
-  const [showAddComponet, setShowAddComponet] = useState(false);
-  const onAdd = () => {
-    addComment(id, childComment);
-    setChildComment("");
-    setShowAddComponet(false);
-  };
-  return (
-    <div className="Comment">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <div style={{ textAlign: "left" }}>{commentText}</div>
-        &nbsp;
-        {childCommments.length > 0 && (
-          <button onClick={() => setShow((show) => !show)}>
-            {show ? "Hide" : "Show"}
-          </button>
-        )}
-      </div>
-      <div>
-        <div>
-          {showAddComponet ? (
-            <>
-              <input
-                type="text"
-                value={childComment}
-                onChange={(e) => setChildComment(e.target.value)}
-                placeholder="add comment"
-              />{" "}
-              <button onClick={onAdd}>Submit</button>
-            </>
-          ) : (
-            <button
-              style={{ cursor: "pointer", fontSize: "0.7rem", color: "black" }}
-              onClick={() => setShowAddComponet(true)}
-            >
-              Reply
-            </button>
-          )}
-        </div>
-      </div>
-      {show &&
-        childCommments.map((childCommentEl, key) => {
-          return (
-            <Comment
-              key={key}
-              comment={childCommentEl}
-              addComment={addComment}
-            />
-          );
-        })}
-    </div>
-  );
-};
 
 export default App;
